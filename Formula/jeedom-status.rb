63fda22479b25d4b4012c51c2a75c0defac01558
Pierre Mavro (deimosfr) <deimosfr@gmail.com>
2020-05-10 22:10:49 +0200

    Brew formula update for jeedom-status version v0.3.2

--- Formula/jeedom-status.rb
+++ Formula/jeedom-status.rb
@@ -2,16 +2,16 @@
 class JeedomStatus < Formula
   desc "Jeedom global status for operating systems status bars"
   homepage "https://github.com/deimosfr/jeedom-status"
-  version "0.3.1"
+  version "0.3.2"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/deimosfr/jeedom-status/releases/download/v0.3.1/jeedom-status_0.3.1_darwin_amd64.tar.gz"
-    sha256 "016d849efaa4f0fe1f6632fdcd5e5dcfa8e48205d0b61839c4346a06749e7e03"
+    url "https://github.com/deimosfr/jeedom-status/releases/download/v0.3.2/jeedom-status_0.3.2_darwin_amd64.tar.gz"
+    sha256 "d7dc4a713c89fd5a97b79e12b97188ecc27c3083d1d2982be5853e826fedc6a4"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/deimosfr/jeedom-status/releases/download/v0.3.1/jeedom-status_0.3.1_linux_amd64.tar.gz"
-      sha256 "9488bca271228b429089da2510407f5ae0d762fdf03dbe76f2704bc731664818"
+      url "https://github.com/deimosfr/jeedom-status/releases/download/v0.3.2/jeedom-status_0.3.2_linux_amd64.tar.gz"
+      sha256 "f738b942e3110b3f3efa5e6d511afde3ff6d0f95bd8e6e039bd810ed19d1ff60"
     end
   end
 
