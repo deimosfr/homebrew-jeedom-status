671013699e1032494dcc85f6e635270908f34c19
Pierre Mavro (deimosfr) <deimosfr@gmail.com>
2021-03-13 14:12:04 +0100

    Brew formula update for jeedom-status version v1.0.0

--- Formula/jeedom-status.rb
+++ Formula/jeedom-status.rb
@@ -10,15 +10,19 @@ class JeedomStatus < Formula
 
   if OS.mac? && Hardware::CPU.intel?
     url "https://github.com/deimosfr/jeedom-status/releases/download/v1.0.0/jeedom-status_1.0.0_darwin_amd64.tar.gz"
-    sha256 "1a5b6e8b11fff3ed0fb508e88751bc497cccb95bc9723099945b2ccc00af4b77"
+    sha256 "f7505dc428397c8ae9fba0029039278bd3deaeb3c318c962a687ef908153cecc"
+  end
+  if OS.mac? && Hardware::CPU.arm?
+    url "https://github.com/deimosfr/jeedom-status/releases/download/v1.0.0/jeedom-status_1.0.0_darwin_arm64.tar.gz"
+    sha256 "aa5f69b3bff23a796a55ac8bfee2cd7decd0a12ff0678ffe9ff71edbc0d18e41"
   end
   if OS.linux? && Hardware::CPU.intel?
     url "https://github.com/deimosfr/jeedom-status/releases/download/v1.0.0/jeedom-status_1.0.0_linux_amd64.tar.gz"
-    sha256 "e91d81bc3ea29e9ed3c1147f7bd315f553b30bf82e263577a19efaefa905f25e"
+    sha256 "56004d945a33e0ab20bc52ae168e7478d52ad1be518c90855c5e899e716aad17"
   end
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
     url "https://github.com/deimosfr/jeedom-status/releases/download/v1.0.0/jeedom-status_1.0.0_linux_arm64.tar.gz"
-    sha256 "20a3b8374ef48863172727c38f02fa65aab3f734f867d9543f2f450240f7100e"
+    sha256 "387faef018b5d694527b4cc6f660945c47326cb411be5e37b21e1d6d0bdee4b6"
   end
 
   def install
