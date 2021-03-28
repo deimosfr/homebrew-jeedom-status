71d0626eb23712f0e76321d4418f08d2d04c8ea9
Pierre Mavro (deimosfr) <deimosfr@gmail.com>
2021-03-28 11:40:16 +0200

    Brew formula update for jeedom-status version v1.0.2

--- Formula/jeedom-status.rb
+++ Formula/jeedom-status.rb
@@ -5,24 +5,24 @@
 class JeedomStatus < Formula
   desc "Jeedom global status for operating systems status bars"
   homepage "https://github.com/deimosfr/jeedom-status"
-  version "1.0.1"
+  version "1.0.2"
   bottle :unneeded
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/deimosfr/jeedom-status/releases/download/v1.0.1/jeedom-status_1.0.1_darwin_amd64.tar.gz"
-    sha256 "d15bbce6edc54496c113642586bd11c6ca275118b5435cb1af4878b553116f45"
+    url "https://github.com/deimosfr/jeedom-status/releases/download/v1.0.2/jeedom-status_1.0.2_darwin_amd64.tar.gz"
+    sha256 "45755e806882b8e6dcf86906a91ce2682bee1c137e1dd1f090e85ac655016b7b"
   end
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/deimosfr/jeedom-status/releases/download/v1.0.1/jeedom-status_1.0.1_darwin_arm64.tar.gz"
-    sha256 "863e675b45663bd13b006afc86beb792b8fb5d51b8131cb9e692a7453c0b922f"
+    url "https://github.com/deimosfr/jeedom-status/releases/download/v1.0.2/jeedom-status_1.0.2_darwin_arm64.tar.gz"
+    sha256 "23c7dd78095da71d8750134f10e20c092a2e4beccb24a56f30aaae26d42033e9"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/deimosfr/jeedom-status/releases/download/v1.0.1/jeedom-status_1.0.1_linux_amd64.tar.gz"
-    sha256 "17c7b95684dc17397c89f9c4d90c7b084e8c57f489dbe23883f98629b41b5fd5"
+    url "https://github.com/deimosfr/jeedom-status/releases/download/v1.0.2/jeedom-status_1.0.2_linux_amd64.tar.gz"
+    sha256 "8af9bca7cff297b87a84a61b84080b536ccf073f5b8aedc9ec5691120a86dd21"
   end
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/deimosfr/jeedom-status/releases/download/v1.0.1/jeedom-status_1.0.1_linux_arm64.tar.gz"
-    sha256 "134cfb54a6dfe20a6a6f2819547d83e1719b7aa339f0a7899ee4e7e406e1aa63"
+    url "https://github.com/deimosfr/jeedom-status/releases/download/v1.0.2/jeedom-status_1.0.2_linux_arm64.tar.gz"
+    sha256 "23ce64da1d7119089d90ae45845ba3f425e4a03a298944c6a4f38c1000bade0f"
   end
 
   def install
